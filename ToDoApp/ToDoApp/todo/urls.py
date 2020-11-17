b579447cbfef9d5e28877c29e4507b9ba5da31a8
Gaurav Sharma <gaurav91297@gmail.com>
2020-11-17 22:33:54 +0530

    Initial Commit

--- ToDoApp/ToDoApp/todo/urls.py
+++ ToDoApp/ToDoApp/todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("<h1>Hello World!</h1>")
 
 
 urlpatterns = [
