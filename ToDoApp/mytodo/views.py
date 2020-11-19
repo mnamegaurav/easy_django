d4c3c616e2937c3b789274e6afe932285b9b2e0d
Gaurav Sharma <gaurav91297@gmail.com>
2020-11-19 23:15:15 +0530

    Adding a Home template in our project

--- ToDoApp/mytodo/views.py
+++ ToDoApp/mytodo/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Home View")
+    template_name = 'home.html'
+    context = {'app_name': "ToDo App"}
+    return render(request, template_name, context=context)
